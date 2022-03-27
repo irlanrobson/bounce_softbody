8dc01cd0ed39c3ef745dba04fbee02a8665e4853
Irlan Robson <irlanrobson@users.noreply.github.com>
2022-03-27 20:40:15 -0300

    Sync

--- include/bounce_softbody/common/settings.h
+++ include/bounce_softbody/common/settings.h
@@ -24,13 +24,15 @@
 #include <new>
 #include <float.h>
 
-typedef signed int int32;
-typedef signed short int16;
 typedef signed char	int8;
-typedef unsigned int uint32;
-typedef unsigned short uint16;
+typedef signed short int16;
+typedef signed int int32;
+
 typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
 typedef unsigned long long uint64;
+
 typedef float scalar;
 typedef double scalar64;
 
